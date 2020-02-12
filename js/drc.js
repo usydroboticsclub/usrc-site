a6682c30985a532b07358843987c684e9ee624c7
NatZha <50129222+NatZha@users.noreply.github.com>
2020-02-12 23:38:17 +1100

    drc and images

---
+++ js/drc.js
@@ -0,0 +1,43 @@
+
+function scrollVideo() {
+    var video = $('#v0').get(0),
+        videoLength = video.duration,
+        scrollPosition = $(document).scrollTop();
+    var half_height = $(window).height() / 2;
+
+    video.currentTime = (scrollPosition / ($(document).height() - $(window).height() / 2)) * videoLength;
+}
+
+$(window).scroll(function (e) {
+    scrollVideo();
+});
+
+
+
+var frameNumber = 0, // start video at frame 0
+    // lower numbers = faster playback
+    playbackConst = 800,
+    // get page height from video duration
+    setHeight = document.getElementById("set-height"),
+    // select video element         
+    vid = document.getElementById('v0');
+// var vid = $('#v0')[0]; // jquery option
+
+// dynamically set the page height according to video length
+vid.addEventListener('loadedmetadata', function () {
+    setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+});
+
+
+// Use requestAnimationFrame for smooth playback
+function scrollPlay() {
+    var frameNumber = window.pageYOffset / playbackConst;
+    vid.currentTime = frameNumber;
+    window.requestAnimationFrame(scrollPlay);
+}
+
+window.requestAnimationFrame(scrollPlay);
+
+console.log(window.pageYOffset);
+    // console.log(setHeight.style.height);
+
