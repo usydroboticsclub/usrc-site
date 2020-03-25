291439e1fa6da46ca7b8ddde2bb28863d1c2e3a8
Steven Liu <steeven.liu2@gmail.com>
2020-03-25 21:42:40 +1100

    removed obfuscating div

--- js/visualisation_controller.js
+++ js/visualisation_controller.js
@@ -1,8 +1,3 @@
-var d = document.createElement("div");
-d.style.cssText = `
-position:absolute; top: 0; left: 0;height:100%;width:100%;z-index:20`;
-document.body.appendChild(d);
-
 var pages = [];
 function addPage(div) {
     pages.push(div);
